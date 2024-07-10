8b40398f59588a2a9dad18ea3c0b3bb6e0071d47
VinzOo93 <v.orru@live.fr>
2024-07-10 14:00:24 +0200

    use of autowing instead of constructors

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,34 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /** @var BookRepository */
-    private $bookRepository;
 
-    /** @var MovieRepository */
-    private $filmRepository;
-
-    /**
-     * DefaultController constructor.
-     * @param BookRepository $bookRepository
-     * @param MovieRepository $movieRepository
-     */
-    public function __construct(
-        BookRepository $bookRepository,
-        MovieRepository $movieRepository
-    )
-    {
-        $this->bookRepository = $bookRepository;
-        $this->movieRepository = $movieRepository;
-    }
 
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(MovieRepository $movieRepository, BookRepository $bookRepository)
     {
         return $this->render('default/index.html.twig', [
-            'lastBooks' => $this->bookRepository->getLastFour(),
-            'lastMovies' => $this->movieRepository->getLastFour(),
+            'lastBooks' => $bookRepository->getLastFour(),
+            'lastMovies' => $movieRepository->getLastFour(),
         ]);
     }
 }
